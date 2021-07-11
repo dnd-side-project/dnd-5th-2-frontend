39053de51cbcca5031d421ed20c769d7ac9fa653
Jinhyang <21500171@handong.edu>
2021-07-11 18:52:24 +0900

    Add pod libraries

---
+++ Vitamin/Pods/Target Support Files/Pods-Vitamin-VitaminUITests/Pods-Vitamin-VitaminUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Vitamin_VitaminUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Vitamin_VitaminUITests
+@end
---
+++ Vitamin/Pods/Target Support Files/Pods-VitaminTests/Pods-VitaminTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VitaminTests : NSObject
+@end
+@implementation PodsDummy_Pods_VitaminTests
+@end
