7972a7a95ba31db18a179cd4c985b2f5054180c0
Jinhyang <21500171@handong.edu>
2021-08-06 22:14:28 +0900

    feat: Extension 파일 추가

---
+++ Vitamin/Vitamin/Sources/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Vitamin
+//
+//  Created by Jinhyang on 2021/08/06.
+//
+
+import Foundation
+
+enum Constants {
+  enum Locale: String {
+    case english = "en"
+    case korean = "ko"
+  }
+}
