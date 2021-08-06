ec21dbc7065a8f05bc891633b5d67255e23e765e
Jinhyang <21500171@handong.edu>
2021-08-07 00:37:28 +0900

    feat: 토큰 생성 함수 추가

--- Vitamin/Vitamin/Sources/Service/NetworkManager.swift
+++ Vitamin/Vitamin/Sources/Service/NetworkManager.swift
@@ -7,23 +7,24 @@
 
 import Foundation
 import Alamofire
+import JWTDecode
 
 class NetworkManager {
-
+  
   static let shared = NetworkManager()
-
+  
   let jsonDecoder = JSONDecoder()
-
+  
   private init() { }
-
+  
   func requestSignUp(with user: User,
                      completionHandler: @escaping (Result<User, Error>) -> Void) {
-
+    
     let signUpURL = URLMaker.makeRequestURL(feature: .signUp)
     let request = AF.request(signUpURL,
                              method: .post,
                              parameters: user)
-
+    
     request.responseDecodable { (response: DataResponse<User, AFError>) in
       switch response.result {
       case .success(let user):
@@ -33,26 +34,26 @@ class NetworkManager {
       }
     }
   }
-
+  
   func requestLogin(with loginUser: LoginUser,
                     completionHandler: @escaping (Bool) -> Void) {
-
+    
     let loginURL = URLMaker.makeRequestURL(feature: .login)
     let request = AF.request(loginURL,
                              method: .post,
                              parameters: loginUser)
-
+    
     request.responseJSON { response in
       switch response.result {
       case .success:
         guard let tokenData = response.value as? [String: String],
-              let token = tokenData["jwt"] else {
+              let jwt = tokenData["jwt"] else {
           completionHandler(false)
           return
         }
-
-        self.generateToken(token: token)
-
+        
+        let _ = self.generateToken(jwt: jwt)
+        
       case .failure(let error):
         print(error.localizedDescription)
         completionHandler(false)
@@ -60,8 +61,15 @@ class NetworkManager {
     }
     completionHandler(true)
   }
-
-  func generateToken(token: String) {
-
+  
+  func generateToken(jwt: String) -> Bool {
+    do {
+      let jwt = try decode(jwt: jwt)
+      
+      return true
+    } catch let error {
+      print(error.localizedDescription)
+      return false
+    }
   }
 }
