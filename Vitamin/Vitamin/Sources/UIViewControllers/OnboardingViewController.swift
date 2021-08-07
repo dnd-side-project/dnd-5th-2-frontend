684f17d361c4d64d4216a768b07e398dc207b1c6
innieminnie <rkddlsl1001@gmail.com>
2021-08-07 20:34:48 +0900

    feat: Onboarding 관련 Storyboard와 ViewController 생성 및 연결

---
+++ Vitamin/Vitamin/Sources/UIViewControllers/OnboardingViewController.swift
@@ -0,0 +1,28 @@
+//
+//  OnboardingViewController.swift
+//  Vitamin
+//
+//  Created by 강인희 on 2021/08/07.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
