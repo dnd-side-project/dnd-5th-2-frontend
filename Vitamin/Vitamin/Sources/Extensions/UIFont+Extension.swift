0ddd5a2935634c3b293f7e281a4b6bd36b966943
innieminnie <rkddlsl1001@gmail.com>
2021-08-08 01:17:31 +0900

    feat: UIFont Extension 추가

--- Vitamin/Vitamin/Sources/Extensions/UIFont+Extension.swift
+++ Vitamin/Vitamin/Sources/Extensions/UIFont+Extension.swift
@@ -7,9 +7,53 @@ import UIKit
 
 extension UIFont {
 
-    /// 폰트
-    class func font(_ font: Font) -> UIFont? {
-        return font.font
-    }
+  /// 폰트
+  class func font(_ font: Font) -> UIFont? {
+    return font.font
+  }
 
+}
+
+extension UIFont {
+  class func Pretendard(type: PretendardType, size: CGFloat) -> UIFont! {
+    guard let font = UIFont(name: type.name, size: size) else {
+      return nil
+    }
+    return font
+  }
+
+  public enum PretendardType {
+    case Black
+    case Bold
+    case ExtraBold
+    case ExtraLight
+    case Light
+    case Medium
+    case Regular
+    case Semibold
+    case Thin
+
+    var name: String {
+      switch self {
+      case .Black:
+        return "Pretendard-Black"
+      case .Bold:
+        return "Pretendard-Bold"
+      case .ExtraBold:
+        return "Pretendard-ExtraBold"
+      case .ExtraLight:
+        return "Pretendard-ExtraLight"
+      case .Light:
+        return "Pretendard-Light"
+      case .Medium:
+        return "Pretendard-Medium"
+      case .Regular:
+        return "Pretendard-Regular"
+      case .Semibold:
+        return "Pretendard-SemiBold"
+      case .Thin:
+        return "Pretendard-Thin"
+      }
+    }
+  }
 }
