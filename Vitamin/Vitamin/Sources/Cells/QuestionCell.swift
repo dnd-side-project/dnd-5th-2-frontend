4f02d61ccdd34bff6e94fd8b60c853cb3c5beada
innieminnie <rkddlsl1001@gmail.com>
2021-08-09 02:37:58 +0900

    feat: OnboardingQuizViewController의 화면 UI 구성

--- Vitamin/Vitamin/Sources/Cells/QuestionCell.swift
+++ Vitamin/Vitamin/Sources/Cells/QuestionCell.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+
+protocol QuestionCellDelegate: AnyObject {
+  func setQuestionView(with text: String)
+  func showAnswerView()
+}
+
+class QuestionCell: UITableViewCell, QuestionCellDelegate {
+  @IBOutlet weak var questionView: UIView!
+  @IBOutlet weak var answerView: UIView!
+  @IBOutlet weak var questionTextLabel: UILabel!
+
+  func setQuestionView(with text: String) {
+    questionView.makeRounded(radius: questionView.frame.height / 2)
+    questionView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+    questionTextLabel.text = text
+  }
+
+  func showAnswerView() {
+    answerView.isHidden = false
+    answerView.makeRounded(radius: answerView.frame.height / 2)
+    answerView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+  }
+}
---
+++ Vitamin/Vitamin/Sources/UIViewControllers/OnboardingQuizViewController.swift
@@ -0,0 +1,73 @@
+//
+//  OnboardingQuizViewController.swift
+//  Vitamin
+//
+//  Created by 강인희 on 2021/08/08.
+//
+
+import UIKit
+
+class OnboardingQuizViewController: UIViewController {
+  @IBOutlet weak var introductionLabel: UILabel!
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var noButton: UIButton!
+  @IBOutlet weak var yesButton: UIButton!
+
+  let quizSet = QuizSet()
+  private var questionSet = [(Quiz, Bool?)]()
+
+  weak var delegate: QuestionCellDelegate?
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setIntroductionLabel()
+    setButtons()
+    questionSet.append((quizSet.quiz(in: 0), nil))
+  }
+
+  @IBAction func tappedYesButton(_ sender: UIButton) {
+    questionSet[0].1 = true
+    self.delegate = tableView.cellForRow(at: [0, 0]) as! QuestionCell
+    delegate?.showAnswerView()
+  }
+
+  @IBAction func tappedNoButton(_ sender: UIButton) {
+    questionSet[0].1 = false
+    self.delegate = tableView.cellForRow(at: [0, 0]) as! QuestionCell
+    delegate?.showAnswerView()
+  }
+
+  private func setIntroductionLabel() {
+    introductionLabel.text = "좋아요. 이제\n아영님에 대해 알아볼게요."
+    introductionLabel.numberOfLines = 2
+    introductionLabel.font = UIFont.Pretendard(type: .Medium, size: 23)
+  }
+
+  private func setButtons() {
+    noButton.layer.borderWidth = 1.0
+    noButton.layer.borderColor = UIColor(red: 225/255, green: 225/255, blue: 232/255, alpha: 1).cgColor
+    noButton.setTitleColor(.black, for: .normal)
+    noButton.makeRounded(radius: noButton.frame.height / 2)
+    noButton.setTitle("아니오", for: .normal)
+
+    yesButton.layer.borderWidth = 1.0
+    yesButton.layer.borderColor = UIColor(red: 225/255, green: 225/255, blue: 232/255, alpha: 1).cgColor
+    yesButton.setTitleColor(.black, for: .normal)
+    yesButton.makeRounded(radius: noButton.frame.height / 2)
+    yesButton.setTitle("네", for: .normal)
+  }
+}
+extension OnboardingQuizViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return questionSet.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let questionCell = tableView.dequeueReusableCell(withIdentifier: "QuestionCell", for: indexPath) as! QuestionCell
+    let currentQuestion = questionSet[indexPath.row]
+
+    self.delegate = questionCell
+    delegate?.setQuestionView(with: currentQuestion.0.question)
+    return questionCell
+  }
+}
