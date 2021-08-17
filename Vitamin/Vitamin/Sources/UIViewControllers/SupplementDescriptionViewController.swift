f0584862dd1c2b2b8ff3a7620c684b0462b27a6d
innieminnie <rkddlsl1001@gmail.com>
2021-08-17 15:38:38 +0900

    feat: setImageViews 메소드 기능 구현

--- Vitamin/Vitamin/Sources/UIViewControllers/SupplementDescriptionViewController.swift
+++ Vitamin/Vitamin/Sources/UIViewControllers/SupplementDescriptionViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class SupplementDescriptionViewController: UIViewController {
 
   @IBOutlet weak var imageScrollView: UIScrollView!
+  @IBOutlet weak var imagesStackView: UIStackView!
   @IBOutlet weak var imagePageControl: UIPageControl!
 
   var imageViews = [UIImageView]()
@@ -19,6 +20,18 @@ class SupplementDescriptionViewController: UIViewController {
     super.viewDidLoad()
     imageScrollView.delegate = self
     setPageControl()
+    setImagesStackView()
+  }
+
+  private func setImagesStackView() {
+    for imageIndex in 0 ..< images.count {
+     let imageView = UIImageView()
+      let xPos = imageScrollView.frameLayoutGuide.layoutFrame.width * CGFloat(imageIndex + 4)
+      imageView.frame = CGRect(x: xPos, y: 0, width: imageScrollView.frameLayoutGuide.layoutFrame.width, height: 200)
+      imageView.image = images[imageIndex]
+      imageView.contentMode = .scaleAspectFit
+      imagesStackView.addArrangedSubview(imageView)
+    }
   }
 
   private func setPageControl() {
